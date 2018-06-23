604ff3faf820b3256981e88fbfe762c3c5ccb3a3
david.gomez <david.gomez@beezy.net>
2018-06-23 15:04:31 +0200

    Create app

---
+++ src/server/api.js
@@ -0,0 +1,23 @@
+const airportsData = require('../data/airports.json');
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+function search(query) {
+	return function(element) {
+		for(var i in query) {
+			if(query[i] != element[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
+
+app.get('/airports', (req, res) => {
+	console.log('query' + JSON.stringify(req.query));
+	res.send(airportsData.filter(search(req.query)));
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
