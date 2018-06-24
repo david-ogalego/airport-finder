23524cc91efa7fae92ca277947b482a0fe38b6dc
david.gomez <david.gomez@beezy.net>
2018-06-24 17:16:11 +0200

    Implement Table component and simple ListAirports container

--- src/client/App.js
+++ src/client/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Nav from './components/Nav';
 import Sidebar from './containers/Sidebar';
+import ListAirports from './containers/ListAirports';
 import styles from './App.sass';
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
 		<div className={styles.container} >
 			<Nav />
 			<Sidebar />
+			<ListAirports />
 		</div>
     );
 }
