1fec784a95af7ce8eded26e69bfcd2d3a31d60ba
David Gómez <david.gomez@beezy.net>
2018-06-26 12:23:57 +0200

    Add path dependency

--- server/api.js
+++ server/api.js
@@ -1,6 +1,7 @@
 const airportsData = require('./data/airports.json');
 const countriesData = require('./data/countries.json');
 const express = require('express');
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 5000;
